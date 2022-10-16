95404e59b57b961b0417fcd53a952ad4f2c8dde6
Fernando Ayats <ayatsfer@gmail.com>
2022-10-16 12:07:52 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -2,50 +2,5 @@
 
 from setuptools import setup
 
-# from resolve_march_native.version import VERSION_STR
-
-
-# def _read(filename):
-#     with open(filename) as f:
-#         return f.read()
-
 if __name__ == "__main__":
     setup()
-
-
-# if __name__ == '__main__':
-#     setup(
-#         description='Yet another Nix cli Help utility',
-#         license='MIT',
-#         # version=VERSION_STR,
-#         author='Fernando Ayats',
-#         author_email='ayatsfer@gmail.com',
-#         url='https://github.com/viperML/nh',
-#         python_requires='>=3.9',
-#         packages=[
-#                 'nh',
-#         ],
-#         entry_points={
-#             'console_scripts': [
-#                 'nh = nh.commands:cli',
-#             ],
-#         },
-#         classifiers=[
-#             # 'Development Status :: 4 - Beta',
-#             # 'Environment :: Console',
-#             # 'Intended Audience :: End Users/Desktop',
-#             # 'Intended Audience :: System Administrators',
-#             # 'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
-#             # 'Natural Language :: English',
-#             # 'Operating System :: POSIX :: Linux',
-#             # 'Programming Language :: Python',
-#             # 'Programming Language :: Python :: 3',
-#             # 'Programming Language :: Python :: 3.7',
-#             # 'Programming Language :: Python :: 3.8',
-#             # 'Programming Language :: Python :: 3.9',
-#             # 'Programming Language :: Python :: 3.10',
-#             # 'Programming Language :: Python :: 3 :: Only',
-#             # 'Topic :: Software Development :: Compilers',
-#             # 'Topic :: Utilities',
-#         ],
-#     )
