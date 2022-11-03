759ffb9caa8a25ba6c61d530b4c79ad354c18c9f
Fernando Ayats <ayatsfer@gmail.com>
2022-11-03 12:57:42 +0100

    usse subprocess

--- src/nixos.rs
+++ src/nixos.rs
@@ -1,5 +1,5 @@
 use clean_path::Clean;
-use log::info;
+use log::{debug, info, warn};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 
@@ -7,14 +7,34 @@ use crate::interface;
 
 const SYSTEM_PROFILE: &str = "/nix/var/nix/profiles/system";
 
-fn run_command(cmd: &str, dry: bool) -> std::io::Result<()> {
+#[derive(Debug)]
+enum RunError {
+    PopenError,
+    ExitError,
+}
+
+impl From<subprocess::PopenError> for RunError {
+    fn from(_: subprocess::PopenError) -> Self {
+        RunError::PopenError
+    }
+}
+
+fn run_command(cmd: &str, dry: bool) -> Result<(), RunError> {
     // let output = std::process::Command::new()
     // info!("{arg0}");
     info!("{cmd}");
     if !dry {
         let mut argv = cmd.split(" ");
         let arg0 = argv.nth(0).expect("Bad command");
-        let output = std::process::Command::new(arg0).args(argv).spawn()?;
+        let output = subprocess::Exec::cmd(arg0)
+            .args(&argv.collect::<Vec<_>>())
+            .capture()?;
+
+        if ! output.success() {
+            return Err(RunError::ExitError)
+        }
+
+        debug!("{:?}", output);
     };
 
     Ok(())
@@ -40,14 +60,23 @@ impl interface::RebuildArgs {
             "--profile",
             SYSTEM_PROFILE,
             &format!(
-                "{}#{}",
+                "{}#nixosConfigurations.{}.config.system.build.toplevel",
                 &flake_clean.to_string_lossy(),
                 &hostname.to_string_lossy()
             ),
         ]
         .join(" ");
 
-        run_command(&cmd, self.dry);
+        // let foo: Result<(), RunError> = {
+        //     run_command(&cmd, self.dry)?;
+        //     Ok(())
+        // };
+        let output = run_command(&cmd, self.dry);
+
+        match output {
+            Err(why) => warn!("Failed to run command! {:?}", why),
+            Ok(_) => debug!("OK")
+        }
 
         todo!("rebuild not implemented!");
     }
