fdcc0b9321738686137ac5da6dd544d8ee42d0b7
Fernando Ayats <ayatsfer@gmail.com>
2022-11-20 09:45:52 +0000

    path walker

--- src/clean.rs
+++ src/clean.rs
@@ -1,5 +1,6 @@
-use std::fs::{self, DirEntry};
-use std::path::PathBuf;
+use std::ffi::{CString, OsString};
+use std::fs::{self, DirEntry, File};
+use std::path::{PathBuf, Path};
 
 use log::trace;
 
@@ -10,32 +11,47 @@ use crate::{commands::NHRunnable, interface::CleanArgs};
 
 static PROFILES_DIR: &str = "/nix/var/nix/profiles";
 
-
 impl NHRunnable for CleanArgs {
     fn run(&self) -> anyhow::Result<()> {
-        let root = PathBuf::from(PROFILES_DIR);
+        let profiles_path = Path::new(PROFILES_DIR);
 
-        remove_old_generations(root)?;
+        // Clean profiles
+        clean_profile(&profiles_path)?;
+
+        // Clean GC roots
+
+        // Clean store
         // run_command("nix-store --gc", self.dry, None)?;
-
         Ok(())
     }
 }
 
-fn remove_old_generations(path: PathBuf) -> anyhow::Result<()> {
-    trace!("Working with path {path:?}");
+fn clean_profile(path: &Path) -> anyhow::Result<()> {
+    for _dir_entry in fs::read_dir(path)? {
+        let subpath = _dir_entry?.path();
 
-    if path.is_dir() {
-        trace!("Walking");
-        for entry in fs::read_dir(path)? {
-            let subpath = entry?.path();
-            trace!("Going to {:?}", subpath);
-            remove_old_generations(subpath)?;
+        if !readable(&subpath)? {
+            return Ok(());
         }
 
-    } else {
-        trace!("is file");
+        let name = subpath.file_name().expect("FIXME").to_str().expect("FIXME");
+
+        if name.contains("-link") && subpath.is_symlink() {
+            // Is a profile
+            trace!("Wipe-checking {subpath:?}");
+            // Clean profile
+        } else if !subpath.is_symlink() && subpath.is_dir() {
+            // Is a container for profiles
+            clean_profile(&subpath)?;
+        }
     }
 
     Ok(())
+}
+
+fn readable(path: &Path) -> Result<bool, anyhow::Error> {
+    let fname = path.to_str().expect("FIXME");
+    let cstr = CString::new(fname).expect("FIXME");
+    let str_bytes = cstr.into_raw();
+    Ok(unsafe { libc::access(str_bytes, libc::R_OK) } == 0)
 }
