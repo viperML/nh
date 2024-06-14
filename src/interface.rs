6fb30a4d490a98d08628ed93a90a9d23820ef9c8
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-06-14 10:51:39 +0200

    search channel from flake input

--- src/interface.rs
+++ src/interface.rs
@@ -155,12 +155,16 @@ pub struct SearchArgs {
     /// Number of search results to display
     pub limit: u64,
 
-    #[arg(long, short, default_value = "nixos-unstable")]
+    #[arg(long, short)]
     /// Name of the channel to query (e.g nixos-23.11, nixos-unstable)
-    pub channel: String,
+    pub channel: Option<String>,
 
     /// Name of the package to search
     pub query: String,
+
+    #[arg(short, long, env = "FLAKE", value_hint = clap::ValueHint::DirPath)]
+    /// Flake to read what nixpkgs channels to search for
+    pub flake: Option<FlakeRef>,
 }
 
 // Needed a struct to have multiple sub-subcommands
